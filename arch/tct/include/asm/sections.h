9ac14caa27b9418ce81b95e42d991514aa356778
Isa Ansharullah <meriororen@gmail.com>
2011-11-01 19:43:29 +0900

    some headers

---
+++ arch/tct/include/asm/sections.h
@@ -0,0 +1,10 @@
+#ifndef ASM_TCT_SECTIONS_H
+#define ASM_TCT_SECTIONS_H
+
+#include <asm-generic/sections.h>
+
+#ifndef __ASSEMBLY__
+extern char __exception_text_start[], __exception_text_end[];
+#endif
+
+#endif
---
+++ arch/tct/include/asm/unistd.h
@@ -0,0 +1,11 @@
+#if !defined(_ASM_TCT_UNISTD_H) || defined (__SYSCALL)
+#define _ASM_TCT_UNISTD_H
+
+#define __ARCH_WANT_SYSCALL_NO_AT
+#define __ARCH_WANT_SYSCALL_NO_FLAGS
+#define __ARCH_WANT_SYSCALL_OFF_T
+#define __ARCH_WANT_SYSCALL_DEPRECATED
+
+#include <asm-generic/unistd.h>
+
+#endif /* _ASM_TCT_UNISTD_H */
