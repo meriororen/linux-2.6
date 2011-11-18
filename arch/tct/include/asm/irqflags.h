431a638adfcf14bbea02ebe6511a672e48cd0ef7
Isa Ansharullah <meriororen@gmail.com>
2011-11-18 15:43:14 +0900

    adding support for irq flags (1)

--- arch/tct/include/asm/irqflags.h
+++ arch/tct/include/asm/irqflags.h
@@ -1,55 +1,88 @@
+/*
+ * Based on:
+ * arch/microblaze/include/asm/irqflags.h
+ */
+
 #ifndef _TCT_IRQFLAGS_H
 #define _TCT_IRQFLAGS_H
 
+#include <linux/types.h>
+#include <asm/registers.h>
+
 static inline void arch_local_irq_disable(void)
 {
-	//unsigned int ie;
-	asm volatile ( ""
-		/*
-		"rcsr %0, IE\n" \  // disabling irq via IE (LM32)
-		"andi %0, %0, 0xfffe\n" \
-		"wcsr IE, %0\n" \
-		: "=r"(ie) \
-		*/
+	unsigned long tmp;
+	asm volatile ( 
+		" mfs	%0, msr		\n"
+		" nop			\n"
+		" andi	%0, %0, %1	\n"
+		" mts	msr, %0		\n"
+		" nop			\n"
+		: "=r"(tmp)
+		: "i" (~MSR_IE)
+		: "memory"
+	);
+}
+
+static inline void arch_local_irq_save(void)
+{
+	unsigned int flags, tmp;
+	asm volatile (
+		" mfs	%0, msr		\n"
+		" nop			\n"
+		" andi	%1, %0, %2	\n"
+		" mts	msr, %1		\n"
+		" nop			\n"
+		: "=r"(flags), "=r"(tmp)
+		: "i"(~MSR_IE)
+		: "memory"
 	);
 }
 
 static inline unsigned long arch_local_save_flags(void)
 {
-	unsigned long ie;
-	asm volatile ( ""
-		/*
-		"rcsr %0, IE\n" : "=r" 
-		*/
+	unsigned long flags;
+	asm volatile (
+		" mfs	%0, msr		\n"
+		" nop			\n"
+		: "=r" (flags)
+		:
+		: "memory"
 	); 
-	return ie; //retreive saved flags from IE
+	return flags; 
 }
 
-static inline void arch_local_irq_save(void)
-{
-	//unsigned int old_ie, new_ie;
-	asm volatile (
-		"" /* saving local irq flags into IE */
-	);
-}
+
 
 static inline void arch_local_irq_enable(void)
 {
-	//unsigned int ie;
+	unsigned long tmp;
 	asm volatile(
-		"" /* enabling irq via IE */
+		" mfs	%0, msr		\n"
+		" nop			\n"
+		" orr	%0, %0, %1	\n"
+		" mts	msr, %0		\n"
+		" nop			\n"
+		: "=r"(tmp)
+		: "i"(MSR_IE)
+		: "memory"
 	);
 }
 
 static inline void arch_local_irq_restore(unsigned long flags)
 {
-	//unsigned int ie;
-	asm volatile("");
+	asm volatile(
+		" mts	 msr, %0	\n"
+		" nop			\n"
+		: 
+		: "r"(flags)
+		: "memory"
+	);
 }
 
 static inline int arch_irqs_disabled_flags(unsigned long flags)
 {
-	return ((flags & 0x1) == 0);
+	return ((flags & MSR_IE) == 0);
 }
 
 static inline int arch_irqs_disabled(void)
